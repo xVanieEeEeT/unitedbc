aac35cc51a46349cdb99fef5fc562c76356a2acf
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-12 01:31:45 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,95 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = 'U';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame("United. || Broadcast","https://twitch.tv//9ivv")
+    console.log('')
+    console.log('')
+    console.log('╔[════════════════════════════════════════════════════════════════]╗')
+    console.log(`[Start] ${new Date()}`);
+    console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════════════════════════════]╗');
+    console.log(`Logged i as * [ " ${client.user.username} " ]`);
+    console.log('')
+    console.log('Informatins :')
+    console.log('')
+    console.log(`servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Users! [ " ${client.users.size} " ]`);
+    console.log(`channels! [ " ${client.channels.size} " ]`);
+    console.log('╚[════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════]╗')
+    console.log(' Bot Is Online')
+    console.log('╚[════════════]╝')
+    console.log('')
+    console.log('')
+  });
+
+ client.on('message', message => {
+    if (!message.author.id === "410778583682777098","474175378118803466") {
+		return;
+	}
+	if(message.author.id === "410778583682777098","474175378118803466") {
+    if (!message.content.startsWith(prefix)) return;
+    var args = message.content.split(' ').slice(1);
+    var argresult = args.join(' ');
+
+
+    if (message.content.startsWith(prefix + 'setwatch')) {
+    client.user.setActivity(argresult, {type: 'WATCHING'})
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Watching Now: **${argresult}**`)
+  }
+
+
+    if (message.content.startsWith(prefix + 'setlis')) {
+    client.user.setActivity(argresult, {type: 'LISTENING'})
+       console.log('test' + argresult);
+      message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
+  }
+
+
+  if (message.content.startsWith(prefix + 'setname')) {
+    client.user.setUsername(argresult).then
+        message.channel.sendMessage(`Username Changed To **${argresult}**`)
+    return message.reply(".");
+  }
+
+  if (message.content.startsWith(prefix + 'setavatar')) {
+    client.user.setAvatar(argresult);
+     message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+  }
+
+  if (message.content.startsWith(prefix + 'setstream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/9ivv");
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Streaming: **${argresult}**`)
+  }
+  if (message.content.startsWith(prefix + 'setplay')) {
+    client.user.setGame(argresult);
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Playing: **${argresult}**`)
+  }
+	}
+
+ });
+
+client.on('message', message => {
+    if(message.content.startsWith(prefix + 'bc')) {
+      if(!message.member.hasPermissions('ADMINISTRATOR')) return;
+    let args = message.content.split(' ').slice(1).join(' ');
+    message.channel.send(`**:ballot_box_with_check: Sent.**`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    m.send(`${args}\n${m}`);
+    if(message.attachments.first()){
+   m.sendFile(message.attachments.first().url).catch();
+    }
+    message.delete();
+    })
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
